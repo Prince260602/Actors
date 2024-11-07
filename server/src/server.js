73b3d6e4b4fef5e5100bffffe86f062f34bb6036
Prince <princemultaniya89@gmail.com>
2024-11-07 21:47:26 +0530

    frnt dne

--- server/src/server.js
+++ server/src/server.js
@@ -14,7 +14,7 @@ dotenv.config();
 let dbConnection;
 
 let corsOptions = { withCredentials: true, credentials: true };
-const allowlist = ["http://localhost:5173","https://mentor.entrepreneurshipnetwork.net"];
+const allowlist = ["http://localhost:5173","https://mentor.entrepreneurshipnetwork.net","https://actors-six.vercel.app"];
 const corsOptionsDelegate = function (req, callback) {
   if (allowlist.indexOf(req.header("Origin")) !== -1) {
     corsOptions.origin = true;
